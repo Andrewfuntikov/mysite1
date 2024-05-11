6f610c7a67a3feb41a4dee20bdb853f3540bf577
Андрей <andhunter77@gmail.com>
2024-05-11 14:40:08 +0500

    Четвертые изменения

--- mysite/products/models.py
+++ mysite/products/models.py
@@ -1,8 +1,14 @@
+import django
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
 from django.db import models
 # Create your models here.
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, primary_key=True)
     description = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.name
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
@@ -10,3 +16,5 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+
